ce72dccd1533155dd657fbd50984df2ebc992934
luciano.cassettai <luciano.cassettai@gmail.com>
2018-05-15 20:19:27 -0300

    Correccion de nombre

--- f_callback.php
+++ f_callback.php
@@ -4,7 +4,7 @@
     if (isset($_POST['userID'])) {
         $_SESSION['userID'] = $_POST['userID'];
         $_SESSION['email'] = $_POST['email'];
-        $_SESSION['name'] = $_POST['nombre'];
+        $_SESSION['nombre'] = $_POST['nombre'];
         $_SESSION['apellido'] = $_POST['apellido'];
         $_SESSION['genero'] = $_POST['genero'];
         $_SESSION['accessToken'] = $_POST['accessToken'];
